1d6db7b009ab993dbe63bea3646134b32d90e09d
odilovzafarzhon <odilovzafarzhon@gmail.com>
2022-05-26 22:48:10 +0500

    working with the crypto and process.pid

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const sum = 1 + 2;
-console.log(sum)
+// const crypto = require('crypto');
+// const start = Date.now()
+// crypto.pbkdf2('123456','5',1000000, 64, 'sha512', ()=>{
+//     console.log('1 end', Date.now() - start)
+// })
+
+// console.log(process.pid)
+// while(true){
+    
+// }
+
+console.log(process.env)
