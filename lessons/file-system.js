a9686d19cde89383e96444b66f3c1af1de942f95
odilovzafarzhon <odilovzafarzhon@gmail.com>
2022-05-27 22:52:13 +0500

    change file-system.js

--- lessons/file-system.js
+++ lessons/file-system.js
@@ -17,16 +17,69 @@ const path = require('path')
 //     }
 // })
 
-fs.writeFile(path.resolve(__dirname,'text.txt'), '5 qwertygthyujk 7 8 9 10', (err) =>{
-    if(err){
-        throw err
-    }
-    console.log('файл записан')
-})
+// fs.writeFile(path.resolve(__dirname, 'text.txt'), '5 qwertygthyujk 7 8 9 10', (err) => {
+//     if (err) {
+//         throw err
+//     }
+//     console.log('файл записан')
+//     fs.appendFile(path.resolve(__dirname, 'text.txt'), 'добавили в конец', (err) => {
+//         if (err) {
+//             throw err;
+//         }
+//         console.log('файл записан')
+//         fs.appendFile(path.resolve(__dirname, 'text.txt'), 'добавили в конец2', (err) => {
+//             if (err) {
+//                 throw err;
+//             }
+//             console.log('файл записан')
+//         })
+//     })
+// })
 
-fs.appendFile(path.resolve(__dirname,'text.txt'), 'добавили в конец', (err) =>{
-    if(err){
-        throw err;
-    }
-    console.log('файд записан')
-})
+
+// const writeFileAsync = async (path, data) => {
+//     return new Promise((resolve, reject) => fs.writeFile(path, data, (err) => {
+//         if (err) {
+//             return reject(err.message)
+//         }
+//         resolve()
+//     }))
+// }
+
+// const appendFileAsync = async (path, data) => {
+//     return new Promise((resolve, reject) => fs.appendFile(path, data, (err) => {
+//         if (err) {
+//             return reject(err.message)
+//         }
+//         resolve()
+//     }))
+// }
+
+// const readFileAsync = async (path) => {
+//     return new Promise((resolve, reject) => fs.readFile(path, {encoding: 'utf-8'}, (err,data) => {
+//         if (err) {
+//             return reject(err.message)
+//         }
+//         resolve(data)
+//     }))
+// }
+
+// const removeFileAsync = async (path) => {
+//     return new Promise((resolve, reject) => fs.rm(path, (err) => {
+//         if (err) {
+//             return reject(err.message)
+//         }
+//         resolve()
+//     }))
+// }
+
+// writeFileAsync(path.resolve(__dirname, 'text.txt'), 'hello world')
+//     .then(() => appendFileAsync(path.resolve(__dirname, 'text.txt'), ' 123 '))
+//     .then(() => appendFileAsync(path.resolve(__dirname, 'text.txt'), ' 456 '))
+//     .then(() => appendFileAsync(path.resolve(__dirname, 'text.txt'), ' 789 '))
+//     .then(() => readFileAsync(path.resolve(__dirname, 'text.txt')))
+//     .then(data => console.log(data))
+//     .catch(err => console.log('err'))
+
+// removeFileAsync(path.resolve(__dirname,'text.txt'))
+// .then(()=> console.log('succses removed'))
