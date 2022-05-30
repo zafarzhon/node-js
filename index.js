c6f6a7e2b48816fa06edd55d13758ae20ea5e16c
odilovzafarzhon <odilovzafarzhon@gmail.com>
2022-05-30 21:47:53 +0500

    change index.js

--- index.js
+++ index.js
@@ -9,4 +9,29 @@
 //
 // }
 
-console.log(process.env)
+// console.log(process.env)
+
+
+///////
+// const http = require('http')
+
+// const PORT = process.env.PORT || 5000
+
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, {
+//         'Content-type': 'text/html; charset=utf-8'
+//     })
+//     res.writeHead(200,{
+//         'Content-type': 'application/json'
+//     })
+//     if(req.url === '/users'){
+//         return res.end('USERS')
+//     }
+//     if(req.url === '/posts'){
+//         return res.end('POSTS')
+//     }
+//     res.end('<h1>Hello world</h1>')
+// })
+
+// server.listen(PORT,()=> console.log(`server started on PORT ${PORT}`))
+/////////
